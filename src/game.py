be43254663cfb0e426e14a564d67738ded5ecdee
Eyenpi <alinabipour01@gmail.com>
2021-12-28 17:24:59 +0330

    refactoring and fix number of columns

--- src/game.py
+++ src/game.py
@@ -10,39 +10,54 @@ class Game:
         self.AI = 2
         self.turn = 1
         self.end = 1
+        self.tie = 0
+        self.winner = -1
 
     def print_board(self):
         print(self.board)
 
-    def choose_row(self):
-        chosenrow = -1
+    def choose_column(self):
         if self.turn == 1:
-            chosenrow = int(input("Choose your row: "))
-            while not self.validate_move(chosenrow):
-                chosenrow = int(input("Wrong row! Choose your row: "))
+            chosencolumn = int(input(f"Choose your column [0 : {self.width - 1}]: "))
+            while not self.validate_move(chosencolumn):
+                chosencolumn = int(input(f"Wrong column! Choose your column [0 : {self.width - 1}]: "))
         else:
             # We should use minimax here
-            chosenrow = self.AI_choose()
-        return chosenrow
+            chosencolumn = self.ai_choose()
+        return chosencolumn
 
     def ai_choose(self):
-        return np.random.randint(1, self.width)
+        return np.random.randint(0, self.width)
 
-    def validate_move(self, row):
-        return self.height > row > 1 and self.board[0][row] == 0
+    def validate_move(self, column):
+        return self.height > column >= 0 and self.board[0][column] == 0
 
-    def drop_piece(self, row):
+    def drop_piece(self, column):
         last = self.height - 1
-        while self.board[last][row] != 0:
+        while self.board[last][column] != 0:
             last -= 1
-        self.board[last][row] = self.turn
+        self.board[last][column] = self.turn
 
     def toggle_turn(self):
         self.turn = 1 if self.turn == 2 else 2
 
+    def check_tie(self):
+        # if there is no more room to drop pieces then self.end = 0 and self.tie = 1
+        pass
+
+    def check_win(self):
+        # if self.turn wins the game then self.end = 0 and self.winner = self.turn
+        pass
+
     def start_game(self):
         while self.end:
-            chosenrow = self.choose_row()
-            self.drop_piece(chosenrow)
+            chosencolumn = self.choose_column()
+            self.drop_piece(chosencolumn)
             self.print_board()
+            if self.check_win() or self.check_tie():
+                break
             self.toggle_turn()
+        if self.tie == 1:
+            print("tie")
+        else:
+            print(f"{self.winner} wins the game")
